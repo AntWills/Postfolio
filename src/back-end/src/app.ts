d1c2642de74a94734676ee1121896e3208e1b2ad
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-13 11:52:24 -0300

    fix(back-end): build on git v11.

--- src/back-end/src/app.ts
+++ src/back-end/src/app.ts
@@ -1,50 +1,50 @@
-// import Fastify, { FastifyInstance } from "fastify";
-// import fastifyCors from "@fastify/cors";
-// import "@infrastructure/types/fastify";
-// import {
-//   serializerCompiler,
-//   validatorCompiler,
-//   ZodTypeProvider,
-// } from "fastify-type-provider-zod";
-// import { AppComposer } from "@compositionRoot/appComposer";
-// import { configureProvaders } from "@infrastructure/fastify/Providers";
+import Fastify, { FastifyInstance } from "fastify";
+import fastifyCors from "@fastify/cors";
+import "@infrastructure/types/fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { AppComposer } from "@compositionRoot/appComposer";
+import { configureProvaders } from "@infrastructure/fastify/Providers";
 
-// function createApp(): FastifyInstance {
-//   const app = Fastify({
-//     logger: false,
-//   }).withTypeProvider<ZodTypeProvider>();
+function createApp(): FastifyInstance {
+  const app = Fastify({
+    logger: false,
+  }).withTypeProvider<ZodTypeProvider>();
 
-//   app.setValidatorCompiler(validatorCompiler);
-//   app.setSerializerCompiler(serializerCompiler);
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
 
-//   app.register(fastifyCors, {
-//     origin: "https://postfolio.com.br",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   });
+  app.register(fastifyCors, {
+    origin: "https://postfolio.com.br",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
 
-//   const appCompose = new AppComposer();
-//   appCompose.registerRoutes(app);
-//   appCompose.configureFastify(app);
-//   appCompose.registerHandlers();
+  const appCompose = new AppComposer();
+  appCompose.registerRoutes(app);
+  appCompose.configureFastify(app);
+  appCompose.registerHandlers();
 
-//   configureProvaders(app);
+  configureProvaders(app);
 
-//   app.get("/", async () => {
-//     return { message: "Bem-vindo ao Postfolio API" };
-//   });
+  app.get("/", async () => {
+    return { message: "Bem-vindo ao Postfolio API" };
+  });
 
-//   return app;
-// }
+  return app;
+}
 
-// const app = createApp();
+const app = createApp();
 
 export default async function handler(req: any, res: any) {
   try {
-    // await app.ready();
-    // app.server.emit("request", req, res);
+    await app.ready();
+    app.server.emit("request", req, res);
 
-    res.status(200).json({ message: "API online na Vercel 🚀" });
+    // res.status(200).json({ message: "API online na Vercel 🚀" });
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Internal Server Error" });
@@ -52,9 +52,9 @@ export default async function handler(req: any, res: any) {
 }
 
 // Rodar localmente fora da Vercel
-// if (process.env.NODE_ENV === "development") {
-//   const PORT = 8080;
-//   app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
-//     console.log(`Servidor rodando em http://localhost:${PORT}`);
-//   });
-// }
+if (process.env.NODE_ENV === "development") {
+  const PORT = 8080;
+  app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
