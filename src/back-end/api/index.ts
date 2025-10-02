da53790a75116bba6d86e9ba32cab014035d2ecc
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-10-02 11:22:49 -0300

    fix(back-end): build on git v27

--- src/back-end/api/index.ts
+++ src/back-end/api/index.ts
@@ -1,14 +1,25 @@
+// api/index.js
 import { createApp } from "../src/app";
+import awsLambdaFastify from "@fastify/aws-lambda";
+
+const app = createApp();
+const proxy = awsLambdaFastify(app);
 
 export default async function handler(req: any, res: any) {
-  try {
-    const app = createApp();
-    await app.ready();
-    app.server.emit("request", req, res);
-
-    // res.status(200).json({ message: "API online na Vercel 🚀" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
+  return proxy(req, res);
 }
+
+// import { createApp } from "../src/app";
+
+// export default async function handler(req: any, res: any) {
+//   try {
+//     const app = createApp();
+//     await app.ready();
+//     app.server.emit("request", req, res);
+
+//     // res.status(200).json({ message: "API online na Vercel 🚀" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ error: "Internal Server Error" });
+//   }
+// }
